5b6c17d2a6ad14f28159f125c8c4856c07cf280a
Louis Chatain <louischatain123@gmail.com>
2025-10-21 00:20:38 +0200

    gonna delete this project later

--- introproject/api/views.py
+++ introproject/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from base.models import Item
 from .serializers import ItemSerializer
 
@@ -15,3 +16,26 @@ def addItem(request):
     if serializers.is_valid():
         serializers.save()
     return Response(serializers.data)
+
+@api_view(['DELETE'])
+def deleteItem(request):
+    item_id = request.data.get('id')
+
+    if item_id is None:
+        return Response(
+            {'error': 'Missing item ID in request data'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+    
+    try:
+        item = Item.objects.get(id=item_id)
+        item.delete()
+        return Response(
+            {'message': f'Item with ID {item_id} successfully deleted.'},
+            status=status.HTTP_204_NO_CONTENT 
+        )
+    except Item.DoesNotExist:
+        return Response(
+            {'error': f'Item with ID {item_id} not found.'},
+            status=status.HTTP_404_NOT_FOUND
+        )
